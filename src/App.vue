44012db66d216dc84db156527790d892fec605d4
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-30 15:40:58 +0400

    Implementing Dynamic Navigation

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,13 @@
           <div class="navbar-content">
             <div class="logo">VUE-CLI</div>
             <ul class="navbar-list">
-              <li class="navbar-item">
-                <a href="" class="navbar-link">Home</a>
-              </li>
-              <li class="navbar-item">
-                <a href="" class="navbar-link">Exsample</a>
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link
+                  :to="link.url"
+                  :title="link.title"
+                  class="navbar-link"
+                  >{{ link.title }}</router-link
+                >
               </li>
             </ul>
           </div>
@@ -23,7 +25,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        { title: "Home", url: "/" },
+        { title: "Example", url: "/example" },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
