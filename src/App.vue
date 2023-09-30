63936f5868766350d732183e9c679f3ddd4b3e08
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-09-30 15:25:20 +0400

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="wrapper">
-    <header>header</header>
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">VUE-CLI</div>
+            <ul class="navbar-list">
+              <li class="navbar-item">
+                <a href="" class="navbar-link">Home</a>
+              </li>
+              <li class="navbar-item">
+                <a href="" class="navbar-link">Exsample</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
     <router-view></router-view>
     <footer>footer</footer>
   </div>
